b392bd98aef530976f6220f9013cad2b2f91c6d9
kazuyo000207 <kazuyo000207@live.jp>
2023-01-10 20:01:20 +0900

    update

--- NoXiRecorder/client.py
+++ NoXiRecorder/client.py
@@ -11,13 +11,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # init
+    client = {}
     with open(args.setting_path) as f:
         setting = json.load(f)
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client["expert"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client["novice"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         BUFSIZE = setting["record"]["bufsize"]
-        for user in ["expert", "novice"]:
+        for user in ["novice", "expert"]:
+            print(setting["record"][user]["ip"])
             try:
-                client.connect(
+                client[user].connect(
                     (setting["record"][user]["ip"],
                      setting["record"][user]["port"])
                 )
@@ -27,13 +30,17 @@ if __name__ == "__main__":
 
     while True:
         msg = input(">>")
-        client.sendall(msg.encode("utf-8"))
+        client["expert"].sendall(msg.encode("utf-8"))
+        client["novice"].sendall(msg.encode("utf-8"))
 
-        data = client.recv(BUFSIZE)
+        data = client["expert"].recv(BUFSIZE)
+        print(data.decode("UTF-8"))
+        data = client["novice"].recv(BUFSIZE)
         print(data.decode("UTF-8"))
 
         if msg == "exit":
             break
 
     time.sleep(2)
-    client.close()
+    client["expert"].close()
+    client["novice"].close()
--- run.bat
+++
@@ -1,4 +0,0 @@
-python NoXiRecorder/capture.py
-python NoXiRecorder/server.py
-python NoXiRecorder/monitorServerAudio.py
-python NoXiRecorder/monitorServerVideo.py
